5f25cee51c4c0df2f5ee497ef303ab85aed0cd67
xkp-pc <godbeast93@gmail.com>
2019-05-31 18:39:48 +0800

    fix

---
+++ src/main/java/com/xkp/attendance/controller/TransformController.java
@@ -0,0 +1,25 @@
+package com.xkp.attendance.controller;
+
+import com.xkp.attendance.manager.ClockInManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: attendance1
+ * @description:
+ * @author: xkp
+ * @create: 2019-05-30 20:05
+ **/
+@RestController
+public class TransformController {
+
+    @Autowired
+    private ClockInManager clockInManager;
+
+    @GetMapping("/transfer")
+    public String transfer(){
+        clockInManager.transfer();
+        return "解析完成";
+    }
+}
